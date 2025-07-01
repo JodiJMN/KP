e035e375aff96998ee56fbf8c71ee1b06418f7a7
JodiJMN <67138634+JodiJMN@users.noreply.github.com>
2025-07-02 04:19:04 +0700

    Update db.py

--- db.py
+++ db.py
@@ -3,10 +3,10 @@ import mysql.connector
 
 def get_connection():
     return mysql.connector.connect(
-        host=os.environ.get('DB_HOST', 'localhost'),
-        user=os.environ.get('DB_USER', 'root'),
-        password=os.environ.get('DB_PASSWORD', ''),
-        database=os.environ.get('DB_NAME', 'db_claim')
+        host=os.environ.get('DB_HOST'),
+        user=os.environ.get('DB_USER'),
+        password=os.environ.get('DB_PASSWORD'),
+        database=os.environ.get('DB_NAME')
     )
     
 # class Config :
